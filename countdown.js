7550333be9f1c12050d3bc9ed84d03587639e2b2
cghinsberg <78280753+cghinsberg@users.noreply.github.com>
2022-09-02 00:37:45 -0300

    Más correcciones

--- countdown.js
+++ countdown.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "07/26/",
+        dayMonth = "07/05/",
         nextShow = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
